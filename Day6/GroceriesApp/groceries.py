99767f1a2dfe5c8823dec310daf72d3cf35dce3f
irfanahamed69 <79094842+irfanahamed69@users.noreply.github.com>
2021-02-24 18:18:44 +0530

    Add files via upload

---
+++ Day6/GroceriesApp/groceries.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, render_template, Request
+
+app = Flask(__name__)
+
+data = [{
+    'vegetables' : 'carrot',
+     'quantity' : 5
+},
+{
+    'vegetables' : 'Potato',
+     'quantity' : 4
+}]
+
+@app.route('/groceries')
+def groceries():
+    return render_template('groceries.html',var=data)
+
+@app.route('/groceries/<string:veg>', methods=['GET'])
+def user(veg):
+    for val in data:
+        if val['vegetables'] == veg:
+            return render_template('groceries.html',var=[val])
+    else:
+        return (f'User {veg} is not found'), 404
