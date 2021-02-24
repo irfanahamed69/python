d1ec2cfa0000a81a94d530d5cf44b5921008c2ad
irfanahamed69 <79094842+irfanahamed69@users.noreply.github.com>
2021-02-24 17:50:01 +0530

    Update developers.py

--- Day6/FlaskApp/developers.py
+++ Day6/FlaskApp/developers.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, Request
 
 app = Flask(__name__)
 
@@ -14,3 +14,11 @@ data = [{
 @app.route('/developers')
 def developer():
     return render_template('developers.html',var=data)
+
+@app.route('/developers/<string:user>', methods=['GET'])
+def user(user):
+    for val in data:
+        if val['name'] == user:
+            return render_template('developers.html',var=[val])
+    else:
+        return (f'User {user} is not found'), 404
